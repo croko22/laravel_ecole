17d47716042ccb13be351d931e68f197b936bba6
croko22 <kvchambit@gmail.com>
2024-07-04 17:23:52 -0500

    feat: Add login functionality and views

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::middleware(['guest'])->group(function () {
+    Route::view('/login', 'login')->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
 Route::resource('course', CourseController::class);
