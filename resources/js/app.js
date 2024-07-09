00d89441268c8325fd312fd3a82c2c36a59a34b2
croko22 <kvchambit@gmail.com>
2024-07-08 19:50:12 -0500

    chore: Remove Alpine.js because livewire comes with alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,2 @@
 import "./bootstrap";
-
-import Alpine from "alpinejs";
 import "flowbite";
-
-Alpine.start();
-
-// If you want Alpine's instance to be available everywhere.
-window.Alpine = Alpine;
