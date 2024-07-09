a9262930d3f7a836f4bc2320ff858bda259890b5
croko22 <kvchambit@gmail.com>
2024-07-08 22:15:24 -0500

    chore: Update app.blade.php layout template

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div x-init="initFlowbite();"></div>
+    <div class="app-container">
+        <x-navbar />
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
