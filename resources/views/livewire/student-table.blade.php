2d1e881d5f682836498fd5e461ba2bfca205465a
croko22 <kvchambit@gmail.com>
2024-07-13 17:56:18 -0500

    Refactor search component in CRUD views

--- resources/views/livewire/student-table.blade.php
+++ resources/views/livewire/student-table.blade.php
@@ -19,42 +19,7 @@
                             </p>
                         </div>
 
-                        <form class="flex items-center max-w-sm mx-auto" wire:submit="search">
-                            <label for="simple-search" class="sr-only">Search</label>
-                            <div class="relative w-full">
-                                <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                        class="w-4 h-4 text-gray-500 dark:text-gray-400" fill="none"
-                                        viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round"
-                                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                                    </svg>
-                                </div>
-                                <input wire:model="query" type="text" id="simple-search"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Search student..." />
-                                @if (strlen($query) > 0)
-                                    <button wire:click="$set('query', '')" type="button"
-                                        class="absolute inset-y-0 right-0 flex items-center pr-3">
-                                        <svg class="w-4 h-4 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
-                                    </button>
-                                @endif
-                            </div>
-                            <button type="submit"
-                                class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
-                                <span class="sr-only">Search</span>
-                            </button>
-                        </form>
+                        <x-search :query="$query" />
 
                         <div>
                             <div class="inline-flex gap-x-2">
