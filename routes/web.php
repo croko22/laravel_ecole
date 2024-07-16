5c62e877e80a0e4a3d897982d3ca00c0e878b310
croko22 <kvchambit@gmail.com>
2024-07-16 11:58:55 -0500

    refactor: Update Course Show component and view

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,23 @@ use App\Http\Controllers\AuthController;
 use App\Livewire\CourseCrud;
 use App\Livewire\StudentTable;
 use App\Livewire\TeacherTable;
+use App\Livewire\Course\Show as CourseShow;
 use App\Livewire\Attendance\Index as AttendanceIndex;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [CourseController::class, 'index'])->name('dashboard');
 
     Route::get('/course', CourseCrud::class)->name('course');
-    Route::get('/course/{course}', [CourseController::class, 'show'])->name('course.show');
+    Route::get('/course/{course}', CourseShow::class)->name('course.show');
 
-    // Route::group(['middleware' => ['role:admin']], function () {
-    Route::get('/student', StudentTable::class)->name('student');
-    Route::get('/teacher', TeacherTable::class)->name('teacher');
-    // });
+    Route::group(['middleware' => ['role:admin']], function () {
+        Route::get('/student', StudentTable::class)->name('student');
+        Route::get('/teacher', TeacherTable::class)->name('teacher');
+    });
 
-    // Route::group(['middleware' => ['role:teacher']], function () {
-    Route::get('/attendance/{course}', AttendanceIndex::class)->name('attendance');
-    // });
+    Route::group(['middleware' => ['role:teacher']], function () {
+        Route::get('/attendance/{course}', AttendanceIndex::class)->name('attendance');
+    });
 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
