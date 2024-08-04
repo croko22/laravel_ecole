91d6767aef36bb1fef742d3ffc61f6da23910c6a
Kevin <kvchambit@gmail.com>
2024-08-03 22:57:24 -0500

    style: refactor and ui imrpovement, time, date inputs

--- resources/views/livewire/attendance/index.blade.php
+++ resources/views/livewire/attendance/index.blade.php
@@ -1,114 +1,103 @@
 <div class="mt-5">
     <h1>Attendance</h1>
 
-    <div x-data="{ modalOpen: $wire.entangle('modalOpen') }">
-        <button @click="modalOpen =!modalOpen" class="flex items-center button-primary gap-x-2">
-            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path d="M5 12h14" />
-                <path d="M12 5v14" />
-            </svg>
-            New attendance session
-        </button>
-        <div x-show="modalOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
-            aria-modal="true">
-            <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
-                <div x-cloak @click="modalOpen = false" x-show="modalOpen"
-                    x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0"
-                    x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200 transform"
-                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                    class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
+    <div class="mt-5 shadow-md sm:rounded-lg">
+        <div
+            class="flex flex-row flex-wrap items-center justify-between min-w-full pb-4 space-y-4 sm:flex-row sm:space-y-0">
+            <div class="flex items-center space-x-4">
+                <div x-data="{ modalOpen: $wire.entangle('modalOpen') }">
+                    <button @click="modalOpen =!modalOpen" class="flex items-center button-primary gap-x-2">
+                        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M5 12h14" />
+                            <path d="M12 5v14" />
+                        </svg>
+                        New attendance session
+                    </button>
+                    <div x-show="modalOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title"
+                        role="dialog" aria-modal="true">
+                        <div
+                            class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                            <div x-cloak @click="modalOpen = false" x-show="modalOpen"
+                                x-transition:enter="transition ease-out duration-300 transform"
+                                x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                                x-transition:leave="transition ease-in duration-200 transform"
+                                x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                                class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true">
+                            </div>
 
-                <div x-cloak x-show="modalOpen" x-transition:enter="transition ease-out duration-300 transform"
-                    x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                    x-transition:leave="transition ease-in duration-200 transform"
-                    x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="modal">
-                    <div class="flex items-center justify-between space-x-4">
-                        <h1 class="text-xl font-medium text-gray-800 ">Create course</h1>
+                            <div x-cloak x-show="modalOpen"
+                                x-transition:enter="transition ease-out duration-300 transform"
+                                x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                                x-transition:leave="transition ease-in duration-200 transform"
+                                x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                class="modal">
+                                <div class="flex items-center justify-between space-x-4">
+                                    <h1 class="text-xl font-medium text-gray-800 ">Create course</h1>
 
-                        <button @click="modalOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                        </button>
-                    </div>
+                                    <button @click="modalOpen = false"
+                                        class="text-gray-600 focus:outline-none hover:text-gray-700">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
 
-                    <div class="relative w-full max-w-2xl max-h-full p-4">
-                        <div class="p-4 space-y-4 md:p-5">
-
-                            <!-- Modal body -->
-                            <form class="p-4 md:p-5" wire:submit.prevent="createLesson">
-                                <div class="grid grid-cols-2 gap-4 mb-4">
-                                    <div class="col-span-2 sm:col-span-1">
-                                        <label for="price"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
-                                            time:</label>
-                                        <div class="relative max-w-sm">
-                                            <div
-                                                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path fill-rule="evenodd"
-                                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </div>
-
-                                            <input type="time" id="time" wire:model="time"
-                                                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                min="09:00" max="18:00" value="00:00" required />
-                                        </div>
+                                <div class="relative w-full max-w-2xl max-h-full p-4">
+                                    <div class="p-4 space-y-4 md:p-5">
+                                        <!-- Modal body -->
+                                        <form class="p-4 md:p-5" wire:submit.prevent="createLesson">
+                                            <div class="grid grid-cols-2 gap-4 mb-4">
+                                                <div class="col-span-2 sm:col-span-1">
+                                                    <label for="price"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                                                        time:</label>
+                                                    <div class="relative max-w-sm">
+                                                        <div
+                                                            class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                                                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                                fill="currentColor" viewBox="0 0 24 24">
+                                                                <path fill-rule="evenodd"
+                                                                    d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                                                                    clip-rule="evenodd" />
+                                                            </svg>
+                                                        </div>
+                                                        <input type="time" id="time" wire:model="time"
+                                                            class="time-input" min="09:00" max="18:00"
+                                                            value="00:00" required />
+                                                    </div>
+                                                </div>
+                                                <div class="col-span-2 sm:col-span-1">
+                                                    <label for="date"
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                                    <input type="date" id="date" wire:model="date"
+                                                        class="date-input" required />
+                                                </div>
+                                        </form>
+                                        <button type="submit" class="flex button-primary">
+                                            <svg class="w-5 h-5 me-1 -ms-1" fill="currentColor" viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                            Save attendance
+                                        </button>
                                     </div>
-                                    <div class="col-span-2 sm:col-span-1">
-                                        <label for="price"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-                                        <input type="date" id="date" wire:model="date"
-                                            class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            required />
-                                    </div>
-                            </form>
-                            <button type="submit" class="flex button-primary">
-                                <svg class="w-5 h-5 me-1 -ms-1" fill="currentColor" viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                                Save attendance
-                            </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="relative mt-5 overflow-x-auto shadow-md sm:rounded-lg">
-        <div
-            class="flex flex-wrap items-center justify-between px-10 pb-4 space-y-4 flex-column sm:flex-row sm:space-y-0">
-            <label for="table-search" class="sr-only">Search</label>
-            <div class="relative">
-                <div
-                    class="absolute inset-y-0 left-0 flex items-center pointer-events-none rtl:inset-r-0 rtl:right-0 ps-3">
-                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </div>
-                <input type="text" id="table-search"
-                    class="block py-3 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search for items">
-            </div>
             <form wire:submit.prevent="deleteMarked">
-                <button class="py-0 button-danger">
+                <button class="button-danger">
                     <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
